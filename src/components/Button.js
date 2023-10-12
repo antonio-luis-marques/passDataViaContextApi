700b4b8ffb2b5f239497f95260baf1a10f1da000
Marques <sir.a.l.marques@gmail.com>
2023-10-12 17:49:23 +0100

    pass data within components via context api

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,27 +5,29 @@
   Importing the useNameContext function from 
   the '../context/Context' file
 */
-import { useNameContext } from '../context/Context'; 
+import { useNameContext } from '../context/Context';
 
 export default function Button() {
 
   /*
     Using the useNameContext function to get the name context
   */
-    const { name, setName } = useNameContext(); 
+  const { name, setName } = useNameContext();
 
-    // Function to change the name based on its current value
-    const toogleName = () => setName(name === 'Antonio Marques' ? 'Mario Pollo' : 'Antonio Marques');
+  // Function to change the name based on its current value
+  const toogleName = () => setName(name === 'Antonio Marques' ? 'Mario Pollo' : 'Antonio Marques');
 
-    return (
-      
-        /*
+  return (
+
+    <>
+      {/*
           Button that, when clicked, triggers the toogleName function.
-         This function is responsible for changing the name based on its logic.
-        */
-      
-      <button onClick={toogleName}> 
+          This function is responsible for changing the name based on its logic.
+    */}
+
+      <button onClick={toogleName}>
         toogle name
       </button>
-    )
+    </>
+  )
 }
